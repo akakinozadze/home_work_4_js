6a31c945c7f17abe98395e3b918273e847e7d0a0
akakinozadze <145987347+akakinozadze@users.noreply.github.com>
2024-02-06 13:10:59 +0400

    3

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//! 1 //
+
+// let array2 = [14, 150, "css", null, "javascript", 25];
+
+// let result = array2.map(function (item) {
+//   if (typeof item == "Number") {
+//     item = item**2;
+//   } else if (typeof item == "string"){
+//    item = item.toUpperCase ;
+//   }
+//   return item ;
+// });
+
+// console.log(result);
+
+//! 2 //
+// let info = "good day";
+
+// let result = info.slice(4);
+// console.log(result);
+
+//! 3 //
+
+// function fnc (item){
+//     return item.length;
+// }
+
+// let sum= fnc ("hello");
+// console.log(sum);
+
+//! 4 //
