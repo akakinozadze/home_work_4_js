49304cec4f722db33ca946f1abaf67f4760c32cd
akakinozadze <145987347+akakinozadze@users.noreply.github.com>
2024-02-06 14:43:05 +0400

    6

--- js/script.js
+++ js/script.js
@@ -27,7 +27,54 @@
 //     return item.length;
 // }
 
-// let sum= fnc ("hello");
+// let sum = fnc ("hello");
 // console.log(sum);
 
 //! 4 //
+
+// let rightAnsver = "tbilisi";
+
+// let question = prompt("The capital of Georgia ?");
+// function ansver() {
+//     if (question == rightAnsver.toLowerCase()){
+//         console.log(true);
+//     }else {
+//         console.log(false);
+//     }
+// }
+// ansver(ansver);
+
+//! 5 //
+
+// let fruits = ["apple", "mango", "avocado", "kiwi"];
+
+// for (let sum = 0 ; sum < fruits.length; sum++){
+//     console.log(sum);
+// }
+// .... //
+// for ( let x in fruits ){
+//     console.log(x);
+// }
+
+// fruits.splice(2,1,"strawberry")
+// console.log(fruits);
+
+//! 6 //
+
+let array1 = [5, 25, 89, 120, 36];
+// //! დავამატე ბოლოში //
+array1.push("javascript", "python");
+// console.log(array1);
+// //! დავამატე დასაწყისში //
+array1.unshift("html", "css");
+// console.log(array1);
+// for (let sum in array1){
+//     console.log(sum);
+// }
+// //! წავშალე პირველი //
+array1.shift();
+// console.log(array1);
+// //! წავშალე ბოლო //
+array1.pop();
+console.log(array1);
+
